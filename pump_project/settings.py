cdf8018fd547092c62e7452f3fcf2dd0f1351b69
web5bariflo <web5.bariflo@outlook.com>
2025-04-09 11:27:47 +0530

    pumpproject

--- pump_project/settings.py
+++ pump_project/settings.py
@@ -79,13 +79,24 @@ WSGI_APPLICATION = 'pump_project.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pumpdb',                           
+        'USER': 'Vertoxlabs',
+        'PASSWORD':'Vtx@2025',
+        'HOST': 'bcpostgressqlserver.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
